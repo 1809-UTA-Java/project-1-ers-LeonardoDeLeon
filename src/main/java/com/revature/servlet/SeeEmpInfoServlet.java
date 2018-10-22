25e2ee8c4bb66bbfbc980c205215b6ec2aaa078d
Leonardo DeLeon <lqdeleon@gmail.com>
2018-10-22 10:29:04 -0500

    add servlets

--- src/main/java/com/revature/servlet/SeeEmpInfoServlet.java
+++ src/main/java/com/revature/servlet/SeeEmpInfoServlet.java
@@ -1,5 +1,6 @@
 package com.revature.servlet;
 
+import java.util.List;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -8,25 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.revature.model.Users;
+import com.revature.repository.ErsDao;
 @SuppressWarnings("serial")
-public class ManagerServlet extends HttpServlet {
-
+public class SeeEmpInfoServlet extends HttpServlet {
+	ErsDao ers = new ErsDao();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	PrintWriter pw = resp.getWriter();
-		
+		PrintWriter pw = resp.getWriter();
+		List<Users> userList = ers.getEmployees();
+		for (Users u : userList) {
+			pw.println(u.toString());
+		}
 		pw.println("<html><body>");
-		pw.println("<h1>Manager Page</h1><br>");
-		pw.println("<a href='employeeinfo'>Go See Employee Info</a><br>");
-		pw.println("<a href='request'>Go Approve a Request</a><br>");
-		pw.println("<a href='deny'>Go Deny a Request</a><br>");
-		pw.println("<a href='logout'>Logout</a><br>");
+		pw.println("<h1>See Employee Info Page</h1><br>");
+
 		pw.println("<a href='index.html'>home</a><br>");
 		pw.println("</body></html>");
 		pw.close();
 	}
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	}
 
